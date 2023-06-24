a1faecbc3411ea22d6f3ee6d92370b0635422c9c
Yves MARCAIS <ymarcais@paul-f4Ar1s4.clusters.42paris.fr>
2023-06-24 18:04:36 +0200

    delete NaN columns

--- describe.py
+++ describe.py
@@ -9,35 +9,78 @@ class Describe:
 	rows : int = 0
 	dataset_only_digit: np.ndarray = None
 
+	#get data
 	def open_data(self, path):
 		dataset = pd.read_csv(path)
+		return dataset.set_index("Index")
+
+	#count columns
+	def count_column(self, dataset):
+		count = 0
+		for _ in dataset.columns:
+			count += 1
+		return count
+	
+	#count rows
+	def count_rows(self, dataset):
+		count = 0
+		for _ in dataset:
+			count += 1
+		return count
+	
+	#count NaN distribution by column
+	def distribution_NaN(self, dataset):
+		NaN_distribution = []
+		for idx, column in enumerate(dataset.columns):
+			num_NaN = dataset[column].apply(lambda x: pd.isna(x)).sum()
+			if num_NaN > 0:
+				NaN_distribution.append([column, num_NaN])
+		return NaN_distribution
+	
+	#check if columns are filled with digits
+	def check_digits(self, dataset):
+		digit_columns = {}
+		for column in dataset.columns:
+			is_only_digits = dataset[column].apply(lambda x: type(x) == float or type(x) == int)
+			if is_only_digits.all() and dataset[column].notnull().all():
+				digit_columns[column] = True
+			else:
+				digit_columns[column] = False
+		return digit_columns
+
+	#delete NaN: drop columns > 50% of NaN
+	def del_NaN_column(self, dataset):
+		rows = self.count_rows(dataset)
+		NaN_distribution = pd.DataFrame(self.distribution_NaN(dataset))
+		columns_to_drop = []
+		for index, row in NaN_distribution.iterrows():
+			if NaN_distribution[1][index] / rows > 0.5:
+				columns_to_drop.append(NaN_distribution[0][index])
+		dataset.drop(columns=columns_to_drop, inplace=True)
 		return dataset
 
-	def check_features(self, dataset):
-		for _ in dataset.columns:
-			self.column += 1
-		return self.column
-	
-	def check_rows(self, dataset):
-		for _ in dataset.iterrows():
-			self.rows += 1
-		return self.rows
-	
-	def check_digits(self, dataset):
-		is_only_digits = dataset.applymap(lambda x: str(x).isdigit())
-		self.dataset_only_digit = dataset[is_only_digits.all(axis=1)].values
-		return self.dataset_only_digit
-	
+	#delete NaN: drop lines if NaN
+	'''def del_Nan_row(self, dataset, int i = 0)
+		for row in dataset.rows:
+			num_NaN = dataset[column].apply(lambda x: pd.isna(x)).sum()
+			if num_NaN :
+				dataset.drop(i)
+		return dataset'''
+
 def main():
 
 	db = Describe(dataset_only_digit=None)
-	dataset = db.open_data("C:/Users/ymarc/Documents/Python/demenagement/datasets/dataset_test.csv")
-	i = db.check_features(dataset)
-	j = db.check_rows(dataset)
+	dataset = db.open_data("datasets/dataset_test.csv")
+	i = db.count_column(dataset)
+	j = db.count_rows(dataset)
 	k = db.check_digits(dataset)
-	print("Number of Features:", i)
-	print("Number of Rows:", j)
-	print("Dataset with Only Digits:", k)
+	l = db.distribution_NaN(dataset)
+	m = db.del_NaN_column(dataset)
+	#print("Is NaN distribution\n", l)
+	#print("Number of column0:", i)
+	#print("Number of Rows:", j)
+	#print("Dataset with Only Digits:", k)
+	print("del_NaN_column\n", m)
 
 if __name__ == "__main__":
     main()
