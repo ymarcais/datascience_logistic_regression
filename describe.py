ae4f7c040e7dd3a86b97f1cfa0d5606c8797f50b
Yves MARCAIS <ymarcais@paul-f4Ar1s4.clusters.42paris.fr>
2023-06-25 15:54:50 +0200

    warnings to fix lvl 1

--- describe.py
+++ describe.py
@@ -8,6 +8,7 @@ class Describe:
 	column : int = 0
 	rows : int = 0
 	dataset_only_digit: np.ndarray = None
+	cleaned_dataset: np.ndarray = None
 
 	#get data
 	def open_data(self, path):
@@ -23,10 +24,11 @@ class Describe:
 	
 	#count rows
 	def count_rows(self, dataset):
-		count = 0
-		for _ in dataset:
-			count += 1
-		return count
+		rows = 0
+		for _ in dataset.index:
+			rows += 1
+			self.rows = rows
+		return self.rows
 	
 	#count NaN distribution by column
 	def distribution_NaN(self, dataset):
@@ -45,8 +47,8 @@ class Describe:
 			if is_only_digits.all() and dataset[column].notnull().all():
 				digit_columns[column] = True
 			else:
-				digit_columns[column] = False
-		return digit_columns
+				dataset.drop(column, axis=1, inplace=True)
+		return dataset
 
 	#delete NaN: drop columns > 50% of NaN
 	def del_NaN_column(self, dataset):
@@ -56,31 +58,98 @@ class Describe:
 		for index, row in NaN_distribution.iterrows():
 			if NaN_distribution[1][index] / rows > 0.5:
 				columns_to_drop.append(NaN_distribution[0][index])
-		dataset.drop(columns=columns_to_drop, inplace=True)
+		dataset = dataset.drop(columns=columns_to_drop)
 		return dataset
 
 	#delete NaN: drop lines if NaN
-	'''def del_Nan_row(self, dataset, int i = 0)
-		for row in dataset.rows:
-			num_NaN = dataset[column].apply(lambda x: pd.isna(x)).sum()
-			if num_NaN :
-				dataset.drop(i)
-		return dataset'''
+	def del_NaN_row(self, dataset):
+		cleaned_dataset = dataset.dropna(axis=0)
+		return cleaned_dataset
+	
+	#init resume of cleaned_dataset
+	def init_resume(self, cleaned_dataset):
+		index_values = ['Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max']
+		resume = pd.DataFrame(columns=cleaned_dataset.columns, index=index_values)
+		return resume
 
+	def count_resume(self, resume):
+		resume.loc['Count',:] = self.rows
+
+	def mean_resume(self, cleaned_dataset, resume):
+		for column in cleaned_dataset.columns:
+			column_data = cleaned_dataset[column]
+			column_sum = sum(column_data)
+			mean = column_sum / self.rows
+			resume.loc['Mean', column] = mean
+		print("mean = \n", mean)
+	
+	def std_resume(self, cleaned_dataset, resume):
+		for column in cleaned_dataset.columns:
+			mean = resume.loc['Mean', column]
+			deviation_sum = 0
+			for index, row in cleaned_dataset.iterrows():
+				deviation_sum += (row[column] - mean) ** 2
+				variance = deviation_sum / self.rows
+				std = variance ** 0.5
+				resume.loc['Std', column] = std
+
+	def min_resume(self, cleaned_dataset, resume):
+		for column in cleaned_dataset.columns:
+			min = cleaned_dataset.iloc[1][column]
+			for index, row in cleaned_dataset.iterrows():
+				if row[column] < min:
+					min = row[column]
+			resume.loc['Min', column] = min
+
+	def max_resume(self, cleaned_dataset, resume):
+		for column in cleaned_dataset.columns:
+			min = cleaned_dataset.iloc[1][column]
+			for index, row in cleaned_dataset.iterrows():
+				if row[column] > min:
+					min = row[column]
+			resume.loc['Max', column] = min
+
+	'''def quartiles_resume(self, cleaned_dataset, resume):
+		for column in cleaned_dataset.columns:
+			values = sorted(cleaned_dataset[column])
+			n = self.rows
+			q25_index = int(n // 4)
+			q50_index = int(n // 2)
+			q75_index = int(3 * n // 4)
+			q25 = values[q25_index]
+			q50 = values[q50_index]
+			q75 = values[q75_index]
+			resume.loc['25%', column] = q25
+			resume.loc['50%', column] = q50
+			resume.loc['75%', column] = q75'''
+
+
+	
 def main():
 
 	db = Describe(dataset_only_digit=None)
 	dataset = db.open_data("datasets/dataset_test.csv")
 	i = db.count_column(dataset)
 	j = db.count_rows(dataset)
-	k = db.check_digits(dataset)
 	l = db.distribution_NaN(dataset)
 	m = db.del_NaN_column(dataset)
+	n = db.del_NaN_row(m)
+	#k = db.check_digits(n)
+	#o = db.init_resume(k)
+	#p = db.count_resume(o)
+	#mean = db.mean_resume(k, o)
+	#std = db.std_resume(k, o)
+	#min = db.min_resume(k, o)
+	#max = db.max_resume(k, o)
+	#quart = db.quartiles_resume(k, o)
 	#print("Is NaN distribution\n", l)
-	#print("Number of column0:", i)
-	#print("Number of Rows:", j)
-	#print("Dataset with Only Digits:", k)
-	print("del_NaN_column\n", m)
+	#print("Number of column:", i)
+	#print("Number of rows:", j)
+	#print("cleaned_dataset:\n", k)
+	#print("del_NaN_column\n", m)
+	#print("resume mean:\n", o)
+	#print("resume std:\n", o)
+	
 
 if __name__ == "__main__":
     main()
